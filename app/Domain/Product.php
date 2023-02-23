ce7bf55e9e33e5458527dc3378e29c3e06d1a38c
fchale <xale1994@gmail.com>
2023-02-23 15:00:33 +0100

    Added domain

---
+++ app/Domain/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'products';
+}
---
+++ app/Domain/Shop.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    protected $table = 'shops';
+}
