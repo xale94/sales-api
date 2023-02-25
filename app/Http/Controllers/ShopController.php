f4ca2f3aa3f3be41dc03533f5802d236bf5340b3
fchale <xale1994@gmail.com>
2023-02-25 13:15:22 +0100

    Add base

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Shop;
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    
+    public function index()
+    {
+        //validar
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Shop $shop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Shop $shop)
+    {
+        //
+    }
+}
