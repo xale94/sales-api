95ef3d5146a09e46b2d71584953ae9521631e1d6
fchale <xale1994@gmail.com>
2023-02-25 16:53:10 +0100

    Fix routing problems in nginx

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::get('/shops', 'ShopController@index');
+Route::get('/shops', 'App\Http\Controllers\ShopController@index');
