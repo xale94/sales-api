2a5d4d984e2401512d52b72a5b9b4771391d5eec
fchale <xale1994@gmail.com>
2023-02-26 13:28:07 +0100

    Added validations

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,6 +9,8 @@ use App\Services\Shops\ShopReadService;
 use App\Services\Shops\ShopSellProductService;
 use App\Services\Shops\ShopUpdateService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class ShopController extends Controller
 {
@@ -21,28 +23,86 @@ class ShopController extends Controller
     public function create(ShopCreateService $service, Request $request)
     {   
         $data = $request->all();
-        return $service->execute($data);
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'products' => 'nullable|array|min:1',
+            'products.*.id' => 'required_with:products|integer',
+            'products.*.name' => 'required_with:products|string|max:255',
+            'products.*.quantity' => 'required_with:products|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(['result' => $service->execute($data), Response::HTTP_ACCEPTED]);
     }
 
-    public function show(ShopReadService $service, int $shopId)
+    public function show(ShopReadService $service, Request $request)
     {
-        return $service->executeFind($shopId);
+        $data = [];
+        $data['shopId'] = $request->route('id');
+        $validator = Validator::make($data, [
+            'shopId' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(['result' => $service->executeFind($data['shopId']), Response::HTTP_ACCEPTED]);
     }
 
     public function update(ShopUpdateService $service, Request $request)
     {
         $data = $request->all();
-        return $service->execute($data);
+
+        $validator = Validator::make($data, [
+            'id' => 'required|integer',
+            'name' => 'required|string|max:255',
+            'products' => 'nullable|array|min:1',
+            'products.*.id' => 'required_with:products|integer',
+            'products.*.name' => 'required_with:products|string|max:255',
+            'products.*.quantity' => 'required_with:products|integer|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(['result' => $service->execute($data), Response::HTTP_ACCEPTED]);
     }
 
-    public function destroy(ShopDeleteService $service, int $shopId)
+    public function destroy(ShopDeleteService $service, Request $request)
     {
-        return $service->execute($shopId);
+        $data = [];
+        $data['shopId'] = $request->route('id');
+
+        $validator = Validator::make($data, [
+            'shopId' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(['result' => $service->execute($data['shopId']), Response::HTTP_ACCEPTED]);
     }
 
     public function buy(ShopSellProductService $service, Request $request)
     {
         $data = $request->all();
-        return $service->execute($data);
+
+        $validator = Validator::make($data, [
+            'shop_id' => 'required|integer',
+            'product_id' => 'required|integer',
+            'quantity' => 'required|integer|min:1'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(['result' => $service->execute($data), Response::HTTP_ACCEPTED]);
     }
 }
